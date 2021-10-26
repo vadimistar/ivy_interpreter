411332c40e5832253aa2ca46d7ae48a0cfbf3c36
Vadimistar <vadimistar@gmail.com>
2021-10-26 14:05:42 +0700

    created basic interface

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-  puts("Hello World!\n");
+  puts( "Ivy (version 0.1)\n"
+        "Type 'q' for quit\n"); 
+  while (1) {
+    printf("> ");
+    fflush(stdout);
+    char inp[256];
+    fgets(inp, sizeof(inp), stdin);
+    if (strcmp(inp, "q\n") == 0) {
+      break;
+    }
+  }
+  return 0;
 }
