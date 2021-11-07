ffbc0d431321a98e4a6ee01173c51f34648db2a1
Vadimistar <vadimistar@gmail.com>
2021-11-07 22:35:55 +0700

    AST structs

---
+++ include/ast.h
@@ -0,0 +1,32 @@
+#ifndef IVY_AST_H
+#define IVY_AST_H
+#include "lexer.h"
+
+#include <stdlib.h>
+
+typedef struct ast_node ast_node;
+
+typedef enum {
+  NULL_AST,
+  INTEGER_AST,
+  BINARY_OP_AST,
+  UNARY_OP_AST,
+} ast_kind;
+
+struct ast_node {
+  char *value; // integer_ast
+  token_kind operand; // binary_op_ast
+  ast_node *left;
+  ast_node *right;
+  ast_kind kind;
+}; 
+
+ast_node *null_ast_init();
+ast_node *ast_init(ast_kind kind);
+
+ast_node *integer_ast_init(char *val);
+
+void ast_free(ast_node *node);
+
+
+#endif//IVY_AST_H
---
+++ src/ast.c
@@ -0,0 +1,29 @@
+#include "../include/ast.h"
+
+ast_node *null_ast_init() {
+  ast_node *node = (ast_node *) calloc(1, sizeof(ast_node));
+  return node;
+}
+
+ast_node *ast_init(ast_kind kind) {
+  ast_node *node = (ast_node *) malloc(sizeof(ast_node));
+  node->kind = kind;
+  return node;
+}
+
+ast_node *integer_ast_init(char *val) {
+  ast_node *node = null_ast_init();
+  node->kind = INTEGER_AST;
+  node->value = val;
+  return node;
+}
+
+void ast_free(ast_node *node) {
+  if (node->left != NULL) {
+    ast_free(node->left); 
+  }
+  if (node->right != NULL) {
+    ast_free(node->right); 
+  }
+  free(node);
+}
